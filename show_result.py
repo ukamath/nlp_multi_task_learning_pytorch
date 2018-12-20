84fdaee726f310387952ad86bb0a1d72b7564c03
sean.narenthiran <sean.narenthiran@digitalreasoning.com>
2018-12-20 14:08:47 +0000

    Added additional fixes to show results

--- show_result.py
+++ show_result.py
@@ -8,9 +8,11 @@ file_paths = [y for x in os.walk(path) for y in glob(os.path.join(x[0], '*.pt'))
 
 results = []
 for file_path in file_paths:
-    if 'result' in file_path:
+    if 'result.pt' in file_path:
         name = file_path.split('/')[-1].split('.')[0]
-        best_accuracy = torch.load(file_path)
-        results.append([name, best_accuracy])
-
-print(results)
+        best_accuracy = torch.load(file_path)['test_accuracies']
+        for val in best_accuracy:
+            if type(val) is tuple or type(val) is list:
+                print(name, ['%0.2f%%' % (float(x) * 100) for x in val])
+            else:
+                print(name, ['%0.2f%%' % (float(val) * 100)
